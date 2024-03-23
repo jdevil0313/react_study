1e53cfd5f85aace42f12772700dc1ff6e4d17181
LEEJUNHO <jdevil0313@gmail.com>
2024-03-23 18:18:09 +0900

    색션 7.Hook, 색션 8. Handing Events, 색션 9. Conditional Rendering, 색션 10. List List and Key 추가.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NotificationList from './chapter_06/NotificationList';
-
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <AttendanceBook />
   </React.StrictMode>
 );
 
